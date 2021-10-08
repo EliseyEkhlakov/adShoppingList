56bddb846ab551700591588a4600a81e9bae9366
Елисей Ехлаков <elisey.ekhlakov@krit.pro>
2021-10-08 17:36:49 +0400

    +viewModel

--- app/src/main/java/com/sumin/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/sumin/shoppinglist/presentation/MainActivity.kt
@@ -2,11 +2,26 @@ package com.sumin.shoppinglist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import com.sumin.shoppinglist.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+    private var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewModel.shopList.observe(this) {
+            Log.d("SHOP_LIST", it.toString())
+            if(count == 0) {
+                val item = it[0]
+                viewModel.deleteShopItem(item)
+                count = 1
+            }
+        }
     }
 }
