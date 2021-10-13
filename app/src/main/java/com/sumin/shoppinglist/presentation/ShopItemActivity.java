09b2956c899adde1a869f68ac0c7c0277120780c
Elisey Ekhlakov <ekhalakovea@bk.ru>
2021-10-13 22:59:00 +0400

    +Second maket

---
+++ app/src/main/java/com/sumin/shoppinglist/presentation/ShopItemActivity.java
@@ -0,0 +1,16 @@
+package com.sumin.shoppinglist.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sumin.shoppinglist.R;
+
+public class ShopItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop_item);
+    }
+}
